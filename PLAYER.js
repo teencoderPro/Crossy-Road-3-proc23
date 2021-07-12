c380ad417f57d935e1f7008ac2bef1fefc3adf60
teencoderPro <85505529+teencoderPro@users.noreply.github.com>
2021-07-12 16:48:47 +0530

    Update PLAYER.js

--- PLAYER.js
+++ PLAYER.js
@@ -5,7 +5,7 @@ constructor(x,y){
     this.spt=createSprite(this.x,this.y,50,50);
     this.spt.shapeColor = "orange";
     this.spt.addAnimation("player",playerAnimation);
-    this.spt.scale = 0.08;
+    this.spt.scale = 0.06;
 }
 move(xdir,ydir){
     this.spt.x += xdir*grid;
